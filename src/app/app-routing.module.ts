4d985f1d2c9ec6986e56559369d331dc7622dc44
Jaime Martínez <jaime.martinez@bootcamp-upgrade.com>
2022-02-19 12:48:11 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'list',
+    component: ProductListComponent
+  },
+  {
+    path: 'list/:id',
+    component: ProductDetailComponent
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListModule } from './product-list/product-list.module';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductComponent } from './pages/product-list/components/product/product.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ProductDetailComponent,
+    ProductListComponent,
+    ProductComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ProductListModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
